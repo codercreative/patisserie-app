8ae739a82aec59222271a5c239db218b89d5eef4
Christina <cligare@hotmail.com>
2024-02-07 15:04:11 -0800

    added cart

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { menuArr } from "./data.js";
 
 const menuEl = document.getElementById("menu");
+const cartEl = document.getElementById("cart");
+const cartSelection = document.getElementById("cart-selection");
 
 const cartArr = [];
 
@@ -16,11 +18,11 @@ function displayMenuHtml(menu) {
     menuHtml += `
       <div class="menu-section" id=${id}>
         <div class="menu-details"> 
-           <p class="emoji">${emoji}</p>
+           <p class="emoji" aria-label=${name}>${emoji}</p>
             <div class="menu-items">
-              <h2>${name}</h2>
+              <h2 class="name">${name}</h2>
               <p>${ingredients.join(", ")}</p>
-              <p>$${price}</p></div>
+              <p class="price">$${price}</p></div>
         </div>
         <button class="add-btn" data-id=${id}>+</button>
       </div>
@@ -29,24 +31,46 @@ function displayMenuHtml(menu) {
   return menuHtml;
 }
 
-//display order
-//clicking on item and adding to order list
+//display cart
+
+//clicking on item and adding to cart
 document.addEventListener("click", (e) => {
   if (e.target.dataset.id) {
     const id = e.target.dataset.id;
     const updateIndex = cartArr.findIndex((item) => item.id == id);
 
+    // Is the item in the cartArr?
     if (updateIndex > -1) {
+      // Add an item to the item already there
       cartArr[updateIndex].quantity += 1;
     } else {
       cartArr.push({ ...menuArr[id], quantity: 1 });
     }
     console.log(cartArr);
+    cartSelection.innerHTML = displayCartHtml(cartArr);
+    cartEl.classList.remove("hidden");
   }
 });
 
+// display cart
 function displayCartHtml(cart) {
   let cartHtml = "";
 
   let totalPrice = 0;
+
+  cart.forEach((item) => {
+    const itemTotal = item.price * item.quantity;
+
+    cartHtml += `
+      <div class="all-items" id=${item.id}>
+        <p class="selected-name">${item.name} (${item.quantity})</p>
+        <p class="selected-total">$${itemTotal}</p>
+      </div>
+    `;
+
+    totalPrice += itemTotal;
+  });
+  document.getElementById("cart-price").innerHTML = `$${totalPrice}`;
+  return cartHtml;
 }
+displayCartHtml(cartArr);
